a126eaf8d2bc1fa90a0a43fb84c61273d0b11484
ahmetsevki <ahmet@nalch.com>
2023-10-03 15:22:36 -0400

    first commit

---
+++ src/main/java/com/example/simplejpa/Ahmet.java
@@ -0,0 +1,10 @@
+package com.example.simplejpa;
+
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class Ahmet {
+    public String hello(){
+        return "Hello World!";
+    }
+}
